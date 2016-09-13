6d86d3ecfff5a50ddef21b51ae9f5d9cddd2ac88
gandaierama <danielbatistagalvao@hotmail.com>
2016-09-13 00:54:55 -0300

    Funções para formatar data

---
+++ data.php
@@ -0,0 +1,27 @@
+﻿<?
+//Data brasil sem hora
+function data_brasil_simples($alvo){
+ $peca_data=explode("-", $alvo);
+ return  $peca_data[2]."/".$peca_data[1]."/".$peca_data[0];
+}
+
+//Data usa sem hora
+function data_usa_simples($alvo){
+ $peca_data=explode("/", $alvo);
+ return  $peca_data[2]."-".$peca_data[1]."-".$peca_data[0];
+}
+
+//Data brasil com hora
+function data_brasil_completa($alvo){
+ $peca = explode(" ", $alvo);
+ $peca_data=explode("-", $peca[0]);
+ return  $peca_data[2]."/".$peca_data[1]."/".$peca_data[0]." - ".$peca[1];
+}
+
+//Data usa com hora
+function data_usa_completa($alvo){
+ $peca = explode(" - ", $alvo);
+ $peca_data=explode("/", $peca[0]);
+ return  $peca_data[2]."-".$peca_data[1]."-".$peca_data[0]." ".$peca[1];
+}
+?>
