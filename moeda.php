b1a0421fd031abbeb34954ef0eac01f3bd358fa1
gandaierama <danielbatistagalvao@hotmail.com>
2016-09-13 00:50:34 -0300

    Alterações para com virgula ou sem

--- moeda.php
+++ moeda.php
@@ -1,6 +1,11 @@
 ﻿<?
-//Moeda
+//Moeda com virgula
 function moeda($alvo){
  return  number_format($alvo, 2, ',', '.');
 }
+//Moeda com ponto
+function moeda2($alvo){
+ return  number_format($alvo, 2, '.', '');
+}
+
 ?>
