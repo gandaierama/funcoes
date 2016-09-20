cb0d39a3be4b31e45bcc40729373f71cad03060f
gandaierama <danielbatistagalvao@hotmail.com>
2016-09-20 16:14:58 -0300

    Https or Http

---
+++ ForceHttpsOrHttp.php
@@ -0,0 +1,23 @@
+﻿<?
+///////////////////////////////////////////////////
+
+function ForceHTTP() {
+    if (@$_SERVER["HTTPS"] == "on") {
+        $url_r = $_SERVER['SERVER_NAME'];
+        $new_url = "http://" . $url_r . $_SERVER['REQUEST_URI'];
+        header("Location: $new_url");
+        exit;
+    }
+}
+
+///////////////////////////////////////////////////
+
+function ForceHTTPS() {
+    if (@$_SERVER["HTTPS"] == "off") {
+        $url_r = $_SERVER['SERVER_NAME'];
+        $new_url = "https://" . $url_r . $_SERVER['REQUEST_URI'];
+        header("Location: $new_url");
+        exit;
+    }
+}
+?>
